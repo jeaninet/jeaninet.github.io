fd7327a3571634aa95ba25b54cca9df90a147e1a
jeaninet <46909170+jeaninet@users.noreply.github.com>
2019-12-02 13:53:28 -0800

    Hopefully fixed the alert

--- assets/scripts/welcome.js
+++ assets/scripts/welcome.js
@@ -30,7 +30,7 @@ document.getElementById("runners").onclick = runnerClicked;
 function runnerClicked() {
   var bodyColor = window.getComputedStyle(document.body, null).getPropertyValue('background-color');
   var movingRunner = window.getComputedStyle(document.getElementById("zero")).getPropertyValue("display");
-  if (bodyColor === "#222" || movingRunner) {
+  if (bodyColor === "#222" || movingRunner == "block") {
     window.alert("Thank you for visiting!");
     return;
   }
